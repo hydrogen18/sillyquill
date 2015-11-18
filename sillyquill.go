3f95b5ffd33e0e7474d82ebe98d6ac16187c1750
Eric Urban <hydrogen18@gmail.com>
2015-11-18 13:42:40 -0600

    basic fixes and improvements. Allow for table mode

--- sillyquill.go
+++ sillyquill.go
@@ -18,6 +18,7 @@ type config struct {
 	Package       string           `toml:"package"`
 	ConnectionMax int              `toml:"connection-max"`
 	Tables        map[string]table `toml:"tables"`
+	TableMode   string `toml:"table-mode"`
 }
 
 func main() {
@@ -38,6 +39,17 @@ func main() {
 
 	if conf.Schema == "" {
 		conf.Schema = "public"
+	}
+	
+	if conf.TableMode == "" {
+	    conf.TableMode = "normal"
+	}
+	
+	var explicit bool
+	
+	if conf.TableMode == "explicit" {
+		explicit = true
+		spicelog.Infof("The selected table mode is explicit. Only generating models for the tables listed in the configuration file.")
 	}
 
 	db, err := sql.Open("postgres", conf.DB)
@@ -67,8 +79,13 @@ func main() {
 		spicelog.Infof("Processing table %q", table.Name())
 
 		tableConf, ok := conf.Tables[table.Name()]
-		if ok {
+		if ok {			
 			if tableConf.Exclude {
+				spicelog.Infof("Skipping table %q", table.Name())
+				continue
+			}
+		} else {
+			if explicit {
 				spicelog.Infof("Skipping table %q", table.Name())
 				continue
 			}
@@ -83,8 +100,10 @@ func main() {
 
 			err = me.Emit(t, conf.OutputDir)
 			if err != nil {
-				spicelog.Errorf("Error processing table %q:%v", table.Name(), err)
-			}
+				spicelog.Errorf("Error processing table %q:%v", t.Name(), err)
+			} else {
+        spicelog.Infof("Emitted model for table %q", t.Name())
+      }
 
 		}(table)
 	}
